f623068dd95107677577337a064e6f494a529ce8
Ivan Korney <shapurid@yandex.ru>
2024-08-17 00:04:14 +0300

    refactor: configs

--- config/runtime.exs
+++ config/runtime.exs
@@ -17,7 +17,7 @@ import Config
 # Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
 if System.get_env("PHX_SERVER") do
-  config :actorrrate, CatOnDutyWeb.Endpoint, server: true
+  config :cat_on_duty, CatOnDutyWeb.Endpoint, server: true
 end
 
 username = System.get_env("LOGIN", "login")
