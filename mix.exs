ac2eec895e4976eb2d703a0769a2dbdde03d6e4a
Ivan Korney <ivkorndev@gmail.com>
2025-01-17 01:31:17 +0300

    bump: v1.6.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CatOnDuty.MixProject do
   def project do
     [
       app: :cat_on_duty,
-      version: "1.5.7",
+      version: "1.6.0",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
